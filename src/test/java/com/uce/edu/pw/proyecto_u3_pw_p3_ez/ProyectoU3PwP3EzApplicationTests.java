bcf727543b32192a35efb64492210a50bbce99b8
Erick Zambrano <beowulfblackman@hotmail.com>
2023-02-02 17:00:02 -0500

    Tarea 11

---
+++ src/test/java/com/uce/edu/pw/proyecto_u3_pw_p3_ez/ProyectoU3PwP3EzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.pw.proyecto_u3_pw_p3_ez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoU3PwP3EzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
